9875a8d2a704e4bb90d958c9960e6b98afe89e9d
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-01-17 23:37:23 -0800

    grunt

---
+++ gruntFile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  grunt.initConfig({
+    jshint: {
+      all: ['app/*.js'],
+      options: {
+        jshintrc: true
+      }
+    },
+
+    jscs: {
+      src: 'app/**/*.js',
+      options: {
+        config: '.jscsrc'
+      }
+    },
+
+    simplemocha: {
+      src: ['test/**/*.js']
+    }
+
+  });
+  grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
+  grunt.registerTask('default', ['test']);
+};
