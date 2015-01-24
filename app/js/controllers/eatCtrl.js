def5b538b3ab2db13b1e96a040fd73d11435b609
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-01-23 17:45:18 -0800

    still tweaking search

--- app/js/controllers/eatCtrl.js
+++ app/js/controllers/eatCtrl.js
@@ -3,15 +3,17 @@
 
 module.exports = function(app) {
   app.controller('mmmCtrl', ["$scope", "mmmBackend", "$http", function($scope, mmmBackend, $http) {
-    $scope.test = "Code rawks my socks!";
     $scope.search = function() {
-      var url = "http://api.bigoven.com/recipe/" + 47725 +
-      "?api_key="+'dvx748HNLFQ2iP293b1YmmE5K5nWkfen';
+      var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&title_kw=" + $scope.mmm.food +
+      "&api_key="+'dvx748HNLFQ2iP293b1YmmE5K5nWkfen';
       $http.get(url).
       success(function(data) {
         var recep = data.Instructions;
-        console.log(recep);
-        $scope.list = recep;
+        var icon = data.object;
+        console.log(icon);
+        $scope.pic = icon;
+
+        $scope.list = "data here";
         return recep;
       }).
       error(function(data) {
